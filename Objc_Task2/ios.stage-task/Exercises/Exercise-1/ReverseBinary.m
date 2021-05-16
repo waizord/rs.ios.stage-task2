cce5d7c78a8af3a7490f41115ef35fa3137c6aab
Ivan <waizord@yandex.ru>
2021-05-16 17:33:49 +0300

    Fix: fixed exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    NSMutableArray * revers = [NSMutableArray new];
+    UInt8 m = 0;
+    for (int i = 0; i < 8; i++) {
+        [revers insertObject:[[NSNumber alloc] initWithInteger: n % 2]  atIndex:0];
+        n = n / 2;
+    }
+    for (int j = 0; j < 8; j++) {
+        if ([[revers objectAtIndex:j] integerValue] == 1) {
+            NSInteger d = [[revers objectAtIndex:j] integerValue] * pow(2, j);
+            m = m + d;
+        }
+    }
+    return m;
 }
