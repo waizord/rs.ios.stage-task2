555d4c877543a5b58bee7ea42809abda31bbd75b
Ivan <waizord@yandex.ru>
2021-05-16 18:12:27 +0300

    Fix: fixed and passed Exercise-2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger result = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (([array[i] integerValue] - [array[j] integerValue]) == [number integerValue]) {
+                result++;
+                NSLog(@"%lu", result);
+            }
+        }
+    }
+    return result;
 }
 
 @end
