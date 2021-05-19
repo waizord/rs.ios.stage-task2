3c57cd912045780a544b3fa39f048a7803ed3cd8
Ivan <waizord@yandex.ru>
2021-05-19 08:38:20 +0300

    Fix: fixed exercise-2 after send at check

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -4,14 +4,30 @@
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     NSInteger result = 0;
-    for (int i = 0; i < array.count; i++) {
-        for (int j = 0; j < array.count; j++) {
-            if (([array[i] integerValue] - [array[j] integerValue]) == [number integerValue]) {
+    NSSet * set = [[NSSet alloc] initWithArray:array];
+    
+    for (NSNumber * item in set) {
+        NSLog(@"First number -> %@", item);
+        
+        for (NSNumber * value in set) {
+            NSLog(@"Second number -> %@", item);
+            if ((NSUInteger)([value integerValue] - [item integerValue]) == [number integerValue]) {
+                NSLog(@"decision = %li", ([value integerValue] - [item integerValue]));
                 result++;
-                NSLog(@"%lu", result);
+                NSLog(@"result = %lu", result);
             }
         }
     }
+//    for (int i = 0; i < array.count; i++) {
+//        //result = 0;
+//        for (int j = 0; j < array.count; j++) {
+//            if (([array[i] integerValue] - [array[j] integerValue]) == [number integerValue]) {
+//
+//                result++;
+//                //NSLog(@"%lu", result);
+//            }
+//        }
+//    }
     return result;
 }
 
